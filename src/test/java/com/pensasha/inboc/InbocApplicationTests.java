6aa64ff644edadeb8f4ee4bf8902ec6430c8eb8d
sobunge <samuelobunge@gmail.com>
2022-11-21 12:14:23 +0300

    first commit

---
+++ src/test/java/com/pensasha/inboc/InbocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pensasha.inboc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InbocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
